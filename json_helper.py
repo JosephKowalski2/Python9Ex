274408e44301af6dba0bd46e6ce89e741fbc17b6
Joseph Kowalski <jkjr25@gmail.com>
2022-03-20 14:26:32 -0400

    Completed exercise 1 part b

--- json_helper.py
+++ json_helper.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 def read_json(file_path):
@@ -6,4 +7,14 @@ def read_json(file_path):
     json_object = json.load(json_open)
     return json_object
 
-print(read_json('/Users/joe/PycharmProjects/Python9Ex/data/super_smash_bros/link.json'))
+def read_all_json_files(file_path):
+    all_json_files = []
+    for file in os.listdir(file_path):
+        full_filename = '%s%s' % (file_path, file)
+        with open(full_filename, 'r') as f:
+            temp_dict = json.load(f)
+            all_json_files.append(temp_dict)
+
+    return all_json_files
+
+print(read_all_json_files('/Users/joe/PycharmProjects/Python9Ex/data/super_smash_bros/'))
